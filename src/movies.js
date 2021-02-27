a5dc38cb7a529b32cfdd730abd51c757db02c897
Laura Kapitza <laura.kapitza@gmail.com>
2021-02-27 17:10:47 +0100

    iteration 5 + 6

--- src/movies.js
+++ src/movies.js
@@ -6,6 +6,7 @@ function getAllDirectors(movies) {
 };
 getAllDirectors(movies);
 
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 
@@ -29,6 +30,7 @@ function ratesAverage(movies) {
 };
 ratesAverage(movies);
 
+
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesRate(movies) {
     //filter all drama movies
@@ -38,17 +40,45 @@ function dramaMoviesRate(movies) {
 };
 dramaMoviesRate(movies);
 
+
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(movie) {
-    const moviesByYear = movie.sort(function(x, y) {
-        return x - y;
+    let copyMovies = movie.slice()
+    const moviesByYear = copyMovies.sort(function(x, y) {
+        //if year is the same,
+        if (x.year === y.year) {
+            return x.title - y.title; //order by title 
+        } else {
+            return x.year - y.year; //otherwise order by year
+        }; 
     });
-    return moviesByYear;
+    return moviesByYear; //return new ordered movie array
 };
 orderByYear(movie);
 
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+function orderAlphabetically(movies) {
+    const movieTitles = movies.map((movie) => movie.title); //map all mive titles
+    const moviesByTitle = movieTitles.sort(); //sort movie titles aphabetically
+    //if there are more than 20 titles, slide the array
+    if (movies.length > 20) {
+        return moviesByTitle.slice(0, 20)
+    }
+    return moviesByTitle;
+}
+orderAlphabetically(movies);
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies) {
+
+}
+turnHoursToMinutes(movies);
+
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
+function bestYearAvg(movies) {
+
+};
+bestYearAvg(movies);
